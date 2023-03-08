1f171f94bd7e76d5ab603f0cbc3c01c56e8a85f0
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-03-08 13:03:40 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://tommimaki97:${password}@cluster0.vazdipe.mongodb.net/Phonebook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// If only password is given, list all entries in the phonebook
+if (process.argv.length === 3) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  // If name and number are given, add a new entry to the phonebook
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
